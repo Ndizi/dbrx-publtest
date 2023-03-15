61300541ecf4955f9a226bece999787cdf9d23eb
janndizi@gmail.com <janndizi@gmail.com>
2023-03-15 13:54:44 +0000

    testing

--- test_read_marketing.py
+++ test_read_marketing.py
@@ -15,13 +15,4 @@ y = df_from_table.columns[-1]
 
 # COMMAND ----------
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df_from_table[X], df_from_table[y], 
-                                                    stratify = df_from_table[y], 
-                                                    shuffle = True, 
-                                                    test_size = 0.2,
-                                                    random_state=2023)
-
-# COMMAND ----------
-
 
